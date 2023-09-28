b0a2808d6918ef12e669878298e7604b39d866ef
Kevin <kevinbodkin2017@u.northwestern.edu>
2023-09-28 15:48:44 -0500

    adding initial support for the logging library. This way we can decide what priority of messages to receive

--- extract_frames.py
+++ extract_frames.py
@@ -14,11 +14,34 @@ from os.path import isdir, sep, split, splitext
 from pathlib import Path
 import argparse
 import sys
+import logging
 
 def ensure_dir_exists(output_dir):
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-def extract_frames_from_video(video_path, output_dir, sample_every, max_frames_to_extract, to_skip=0):
+def extract_frames_from_video(video_path:str, output_dir:str, sample_every, max_frames_to_extract:int, to_skip:int=0, verbosity:str = 'warning'):
+    """
+    extracts frames from a video
+
+        video_path                  full path to video
+        output_dir                  where to put the frames
+        sample_every                how frequently to sample
+        max_frames_to_extract       maximum frames to extract
+        to_skip                     number of frames at beginning to skip
+        verbosity                   how much info do we want? (default:warnings)
+
+
+    """
+
+    # parse verbosity level
+    if verbosity.lower() == 'info':
+        logging.basicConfig(format="%(funcName)s:: %(message)s", level=logging.INFO)
+    elif verbosity.lower() == 'debug': 
+        logging.basicConfig(format="%(funcName)s:: %(message)s", level=logging.DEBUG)
+    else:
+        logging.basicConfig(format="%(funcName)s:: %(message)s", level=logging.WARNING)
+
+
     _, video_name = split(video_path)
     base, ext = splitext(video_name)
     pos = to_skip + (sample_every / 2.) # sample halfway through each range of frames
@@ -26,7 +49,7 @@ def extract_frames_from_video(video_path, output_dir, sample_every, max_frames_t
     try:
         vc = VideoCapture(video_path)
         if not vc.isOpened():
-            print(f"video {video_path} is not readable, skipping")
+            logging.warning(f"video {video_path} is not readable, skipping")
             return
         frames = int(vc.get(CAP_PROP_FRAME_COUNT))
         while pos < frames-1 and extracted < max_frames_to_extract:
@@ -38,12 +61,21 @@ def extract_frames_from_video(video_path, output_dir, sample_every, max_frames_t
                 extracted += 1
             pos += sample_every
     except Exception as e:
-        print(f"extract_frames_from_video encountered exception {e} while working on video {video_path}, frame {int(pos)}")
+        logging.warning(f"extract_frames_from_video encountered exception {e} while working on video {video_path}, frame {int(pos)}")
     finally:
         vc.release()
     return extracted
 
-def extract_frames(input_dir, output_dir, frames_to_extract, to_skip=0):
+def extract_frames(input_dir, output_dir, frames_to_extract, to_skip=0, verbosity:str='warning'):
+    # parse verbosity level
+    if verbosity.lower() == 'info':
+        print('verbosity level set to info')
+        logging.basicConfig(format="%(funcName)s:: %(message)s", level=logging.INFO)
+    elif verbosity.lower() == 'debug': 
+        logging.basicConfig(format="%(funcName)s:: %(message)s", level=logging.DEBUG)
+    else:
+        logging.basicConfig(format="%(funcName)s:: %(message)s", level=logging.WARNING)
+
     total_frames = 0
     ensure_dir_exists(output_dir)
     input_files = glob(input_dir + sep + '*')
@@ -52,34 +84,34 @@ def extract_frames(input_dir, output_dir, frames_to_extract, to_skip=0):
     for f in input_files:
         _, ext = splitext(f)
         if not ext in ('.mp4', '.avi', '.mov', '.mpeg'):
-            print(f'Unsupported file extension "{ext}", skipping {f} on scan pass')
+            logging.warning(f'Unsupported file extension "{ext}", skipping {f} on scan pass')
             continue
         try:
-            print(f"ext is {ext}, trying to open {f}")
+            logging.info(f"Attempting to open {split(f)[-1]}")
+            # print(f"ext is {ext}, trying to open {f}")
             vc = VideoCapture(f)
             if not vc.isOpened():
-                print(f"video {f} is not readable, skipping")
+                logging.warning(f"video {f} is not readable, skipping")
                 continue
             num_frames = int(vc.get(CAP_PROP_FRAME_COUNT))
             total_frames += max(0, num_frames - to_skip)
-            print(f"Adding {num_frames}")
+            logging.info(f"Found {num_frames} potential frames")
         except Exception as e:
-            print(f"extract_frames encountered exception {e} while counting frames of video {f}")
+            logging.error(f"extract_frames encountered exception {e} while counting frames of video {split(f)[-1]}")
         finally:
             vc.release()
 
     sample_every = total_frames / frames_to_extract
 
-    print(f"Total frames: {total_frames}, sampling every {sample_every} frames")
+    logging.info(f"Total frames: {total_frames}, sampling every {sample_every} frames")
 
     # go through the same files again, extracting frames this time
     for f in input_files:
         _, ext = splitext(f)
         if not ext in ('.mp4', '.avi', '.mov', '.mpeg'):
             continue
-        print(f"Extracting no more than {frames_to_extract} frames from {f} ...", end='', flush=True)
-        extracted = extract_frames_from_video(f, output_dir, sample_every, frames_to_extract, to_skip)
-        print(f" got {extracted}.")
+        extracted = extract_frames_from_video(f, output_dir, sample_every, frames_to_extract, to_skip, verbosity=verbosity)
+        logging.info(f"Extracting no more than {frames_to_extract} frames from {split(f)[-1]} ... got {extracted}.")
         frames_to_extract -= extracted
 
 if __name__ ==  '__main__':
@@ -90,6 +122,7 @@ if __name__ ==  '__main__':
         output_dir          Directory/folder to place extracted frames in.  Will be created if needed.
         frames_to_extract   Total number of frames to extract across all videos found
         frames_to_skip      Number of frames to skip at the beginning of each video. Default is 0.
+        verbosity           How much information do you want? Default: warnings and errors
     """
 
     parser = argparse.ArgumentParser(description='extract_frames command line', prog='extract_frames')
@@ -97,6 +130,14 @@ if __name__ ==  '__main__':
     parser.add_argument('output_dir', type=str, help="directory/folder to place extracted frames in.  Will be created if needed.")
     parser.add_argument('frames_to_extract', type=int, help="total number of frames to extract across all videos found")
     parser.add_argument('-s', '--skip', dest='frames_to_skip', type=int, default=0, help="number of frames to skip at the beginning of each video")
+    parser.add_argument('-v', '--verbose', action='count', dest='verbosity', default=0)
     args = parser.parse_args(sys.argv[1:])
 
-    extract_frames(args.input_dir, args.output_dir, args.frames_to_extract, args.frames_to_skip)
+    if args.verbosity == 2:
+        verbosity = 'debug'
+    elif args.verbosity == 1:
+        verbosity = 'info'
+    else:
+        verbosity = 'warning'
+
+    extract_frames(args.input_dir, args.output_dir, args.frames_to_extract, args.frames_to_skip, verbosity)
